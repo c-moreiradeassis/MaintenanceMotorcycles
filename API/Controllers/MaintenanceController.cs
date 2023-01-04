fe684c5ad5f4c2266bb0a05f31fd37dd80ab48d9
Carlos Assis <c.moreiradeassis@gmail.com>
2023-01-04 18:47:23 -0300

    feat: initial commit

---
+++ API/Controllers/MaintenanceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MaintenanceController : ControllerBase
+    {
+        public void Index()
+        {
+
+        }
+    }
+}
